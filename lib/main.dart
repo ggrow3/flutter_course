b4321cff9b180289bbe06be659622c50acd27043
Kevin MacNeel <kevjac@gmail.com>
2019-02-17 22:30:58 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  build(context) {
+    return MaterialApp(
+        home: Scaffold(appBar: AppBar(title: Text('Easy List'))));
+  }
+}
