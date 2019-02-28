61a1db4770a0f3b0534cc7a017187f8aea12a643
Kevin MacNeel <kevjac@gmail.com>
2019-02-27 22:37:16 -0800

    themes

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,18 +3,39 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct) {
+    print('[Products Manager Widget] Constructor');
+  }
+ 
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return null;
+    return _ProductManagerState();
   }
 }
 
-class ProductManagerState extends State<ProductManager> {
+class _ProductManagerState extends State<ProductManager> {
   List<String> _products = ['Food Tester'];
 
   @override
+  void initState() {
+    super.initState();
+    _products.add(widget.startingProduct);
+    
+
+  }
+ 
+ @override
+ void didUpdateWidget(ProductManager oldWidget) {
+   print('[ProductManagerState] didUpdateWidget()');
+   super.didUpdateWidget(oldWidget);
+ }
+
+  @override
   Widget build(BuildContext context) {
+    print('[Products Manager Widget] Constructor');
     return Column(children: [Container(
       margin: EdgeInsets.all(10.0),
       child: RaisedButton(
@@ -25,7 +46,7 @@ class ProductManagerState extends State<ProductManager> {
 
           print(_products);
         },
-        child: Text('Add Products!'),
+        child: Text('Add Products'),
       ),
 
 
--- lib/products.dart
+++ lib/products.dart
@@ -5,22 +5,26 @@ class Products extends StatelessWidget
 
  final List<String> products;
  
- Products(this.products);
+ Products(this.products)
+ {
+   print('[Products Widget] Constructor');
+ }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   
+   print('[Products Widget] Build');
     return Column(
           children: products.map(
             (element) => Card(
                   child: Column(
                     children: <Widget>[
-                      Image.asset('assets/food.jpg'),
+                      //Image.asset('assets/food.jpg'),
                       Text(element)
                     ],
                   ),
-                )).toList(),
+                )
+            ).toList(),
         );
   }
 
